f726f4c25ee2143849a213b3ece17317ae0a0332
vmnekrasovv <vmnekrasov777@gmail.com>
2025-05-04 14:47:58 +0300

    Стили header-center-first, header-center-second

--- app/js/burger.js
+++ app/js/burger.js
@@ -1,14 +1,14 @@
 (function($){
 	$(document).ready(function(){
 
-		$('.burger').append("<span class='burger__element'></span>");
+		// $('.burger').append("<span class='burger__element'></span>");
 
-		let btn = $('.burger, .header-menu');
+		// let btn = $('.burger, .header-menu');
 
-		btn.click(function(){
-			btn.toggleClass('active');
-			$('body').toggleClass('lock');
-		});
+		// btn.click(function(){
+		// 	btn.toggleClass('active');
+		// 	$('body').toggleClass('lock');
+		// });
 
 	});
 })(jQuery);
