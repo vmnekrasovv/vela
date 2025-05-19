4c16da356d48e248dc83c50396292c57892d5cb0
vmnekrasovv <vmnekrasov777@gmail.com>
2025-05-19 03:03:08 +0300

    Основа header-tags-menu

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
 (function($){
 	$(document).ready(function(){ 
 		
+		$('.header-tags__button').on('click', function(e){
+			
+			let tagsButtonDataAttr = $(this).attr('data-attr');
+
+			$('.header-tags-menu').filter(function(index,element){
+				if($(element).attr('data-attr') !== tagsButtonDataAttr){
+					$(element).removeClass('active');
+					return false;
+				} else {
+					$(element).toggleClass('active');
+				}
+			});
+		});
+
 	});
 })(jQuery);
