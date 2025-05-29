f06fd132311e2dacf5cfefd56814bf2311b4c374
vmnekrasovv <vmnekrasov777@gmail.com>
2025-05-29 04:13:52 +0300

    fix_1

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,6 @@ let paths = {
 	scripts: {
 		src: [
 			'node_modules/jquery/dist/jquery.min.js', // npm vendor example (npm i --save-dev jquery)
-			baseDir + '/js/burger.js',
 			baseDir + '/js/app.js' // app.js. Always at the end
 		],
 		dest: baseDir + '/js',
